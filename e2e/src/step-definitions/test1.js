c829118604c1ba3cedb5613c67301123c0230756
Fuad Binzia <31895605+fuadbinzia@users.noreply.github.com>
2023-04-14 15:47:12 -0400

    Delete test1.js

--- e2e/src/step-definitions/test1.js
+++
@@ -1,8 +0,0 @@
-// Write a function that accepts a string as input and
-// returns boolean response if the string is a palindrome or not
-
-
-function palindrome(str)  {
-
-return str === str.split('').reverse().join('');
-}
